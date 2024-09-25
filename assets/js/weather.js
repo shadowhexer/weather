e590d9b35b9771dba0b66aa187030be8b133391c
Hexer <40330756+shadowhexer@users.noreply.github.com>
2024-09-26 07:07:24 +0800

    Update weather.js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -25,7 +25,7 @@ async function checkWeather(city) {
 
         mapIden.src = mapURL + data.coord.lon + '!3d' + data.coord.lat + '!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwNTYnNTcuMSJOIDEyNcKwMzInMzcuMCJF!5e0!3m2!1sen!2sph!4v1707185850993!5m2!1sen!2sph'; // Display Map
 
-        document.querySelector(".city").innerHTML = data.name; // Name of the city
+        document.querySelector(".city").textContent = data.name; // Name of the city
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"; // Temperature
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%"; // Humidity
         document.querySelector(".wind").innerHTML = data.wind.speed + " kph"; // Wind speed
